a9afa25d44635a25e96b3a015fcf1d2dde1afabc
Michael Hebblethwaite <32097294+michebble@users.noreply.github.com>
2021-06-16 22:08:57 +0900

    Create test for each example problem

--- spec/lib/toy_robot_spec.rb
+++ spec/lib/toy_robot_spec.rb
@@ -2,9 +2,13 @@ require "toy_robot"
 
 RSpec.describe ToyRobot do
   subject(:toy_robot) { described_class }
-  let(:commands) { ["PLACE 0,0,NORTH", "MOVE", "REPORT"] }
 
-  it "returns the expected output" do
-    expect(toy_robot.call(commands)).to eq("0,1,NORTH")
+  it "returns the expected output for the problem examples", :aggregate_failures do
+    example_a = ["PLACE 0,0,NORTH", "MOVE", "REPORT"]
+    example_b = ["PLACE 0,0,NORTH", "LEFT", "REPORT"]
+    example_c = ["PLACE 1,2,EAST", "MOVE", "MOVE", "LEFT", "MOVE", "REPORT"]
+    expect(toy_robot.call(example_a)).to eq("0,1,NORTH")
+    expect(toy_robot.call(example_b)).to eq("0,0,WEST")
+    expect(toy_robot.call(example_c)).to eq("3,3,NORTH")
   end
 end
