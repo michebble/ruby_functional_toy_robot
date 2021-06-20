a42ad0504e549c3c1943f87bc7ae9d7d7d60c1df
Michael Hebblethwaite <32097294+michebble@users.noreply.github.com>
2021-06-20 19:26:01 +0900

    Update ToyRobot to use Place and Move
    This is the new way for handling commands for the toy robot, adding them as classes should make adding more easier.

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -1,12 +1,26 @@
-require "data"
-require "report"
+require "pry"
+
+require "place"
+require "move"
 
 class ToyRobot
   class << self
-    def call(commands)
-      Data
-        .call({x: 0, y: 1, direction: :north})
-        .then(&Report)
+    COMMAND_ACTIONS = {
+      "PLACE" => Place,
+      "MOVE" => Move
+    }
+    def call(robot, commands)
+      commands.inject(robot) do |robot, command|
+        COMMAND_ACTIONS
+          .fetch(command.split.first, :return_robot)
+          .then { |action| action.call(robot, command) }
+      end
+    end
+
+    private
+
+    def return_robot
+      proc { |robot, _| robot }
     end
   end
 end
--- spec/lib/toy_robot_spec.rb
+++ spec/lib/toy_robot_spec.rb
@@ -3,12 +3,42 @@ require "toy_robot"
 RSpec.describe ToyRobot do
   subject(:toy_robot) { described_class }
 
-  it "returns the expected output for the problem examples", :aggregate_failures do
-    example_a = ["PLACE 0,0,NORTH", "MOVE", "REPORT"]
-    example_b = ["PLACE 0,0,NORTH", "LEFT", "REPORT"]
-    example_c = ["PLACE 1,2,EAST", "MOVE", "MOVE", "LEFT", "MOVE", "REPORT"]
-    expect(toy_robot.call(example_a)).to eq("0,1,NORTH")
-    expect(toy_robot.call(example_b)).to eq("0,0,WEST")
-    expect(toy_robot.call(example_c)).to eq("3,3,NORTH")
+  it "places" do
+    expect(toy_robot.call({}, ["PLACE 0,0,NORTH"])).to eq({
+      facing: "NORTH",
+      x: 0,
+      y: 0
+    })
   end
+
+  it "moves" do
+    expect(toy_robot.call({}, ["PLACE 0,0,NORTH", "MOVE"])).to eq({
+      facing: "NORTH",
+      x: 0,
+      y: 1
+    })
+  end
+
+  xit "turns" do
+    robot = {
+      facing: "NORTH",
+      x: 0,
+      y: 0
+    }
+
+    expect(toy_robot.call(robot, ["LEFT"])).to eq({
+      facing: "WEST",
+      x: 0,
+      y: 0
+    })
+  end
+
+  # xit "returns the expected output for the problem examples", :aggregate_failures do
+  #   example_a = ["PLACE 0,0,NORTH", "MOVE", "REPORT"]
+  #   example_b = ["PLACE 0,0,NORTH", "LEFT", "REPORT"]
+  #   example_c = ["PLACE 1,2,EAST", "MOVE", "MOVE", "LEFT", "MOVE", "REPORT"]
+  #   expect(tr.call(example_a)).to eq("0,1,NORTH")
+  #   expect(tr.call(example_b)).to eq("0,0,WEST")
+  #   expect(tr.call(example_c)).to eq("3,3,NORTH")
+  # end
 end
